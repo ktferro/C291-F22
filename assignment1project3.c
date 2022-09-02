ccff12ee9511caa0ba9e3990d711e8182075ebfc
ktferro <59977662+ktferro@users.noreply.github.com>
2022-09-01 22:48:21 -0400

    Create assignment1project3.c

---
+++ assignment1project3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() { 
+int numbera, numberb, sum, difference,product,quotient ;
+    
+    if printf("add");
+    sum = numbera + numberb;      
+    printf("%d + %d = %d", numbera, numberb, sum);
+
+    if else printf("subtract");
+    difference = numbera - numberb;
+    printf("%d - %d = %d", numbera, numberb, difference);
+
+    if else printf("multiply");
+    difference = numbera * numberb;
+    printf("%d * %d = %d", numbera, numberb, product);
+
+    if else printf("divide");
+    difference = numbera / numberb;
+    printf("%d /  %d = %d", numbera, numberb, quotient);
