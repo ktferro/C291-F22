6023cead9fa86d50e6dfba3d0cfb9b2135d34012
ktferro <59977662+ktferro@users.noreply.github.com>
2022-09-01 22:33:47 -0400

    Create assignment1project2.c

---
+++ assignment1project2.c
@@ -0,0 +1,8 @@
+void main (){
+   int month, year, age; //Declaring variables as integer
+   printf("Enter current month"); //Asking user to input month
+   printf("Enter current year");
+   printf("Enter your age");
+   scanf("%d %d %d",&month,&year, &age); 
+   printf("It is %d", month, "of%d", year, "you are %d", age,"years old"); //Displaying output   
+
