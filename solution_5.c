c04b58da45ad470b0ba3cbaa94670e2330bd7880
ktferro <59977662+ktferro@users.noreply.github.com>
2022-09-15 21:56:32 -0400

    Create solution_5.c

---
+++ solution_5.c
@@ -0,0 +1,10 @@
+int i, j, n;
+    for (i = 1; i < n; i++) {
+        for (j = 1; j <= n-i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= i*2-1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
